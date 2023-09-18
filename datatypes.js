44ab20c757065b42f93999eceea192b72fa35dfd
yash <ycjaware@gmail.com>
2023-09-18 23:47:19 +0530

    added datatypes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3); //we are using node not browser
+
+let name="Yash"
+let age=18
+let isLoggedIn=false;
+
+//number=2 to power 53
+//bigint
+//boolean
+//null=>standalone value 
+//undefined=>value not defined 
+
+//console.log(b);//not defined becuase it is not declared 
+
+console.log(typeof(age));
+console.log(typeof null );
---
+++ varibles.js
@@ -0,0 +1,10 @@
+const accountId=1445;
+let accountEmail="ycjaware@gmail.com";
+var accountPassword="Yash";//we dont use it now becuase of block scope and functional scope 
+accountCity="Jaipur";
+accountPassword="Jaware"; //this works but dont use it like this
+let accountStte;
+// accountId=1; not allowed
+accountEmail='abc@gmail.com';
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStte]);
+
