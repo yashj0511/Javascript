b5ab49ebbe7105c1d655c67e3687ffd72e044448
yash <ycjaware@gmail.com>
2023-09-19 00:10:37 +0530

    Added datatypes and conversions and operations

---
+++ contersionsOperations.js
@@ -0,0 +1,48 @@
+let score="33abc"
+
+// console.log(typeof score);
+
+let valueInNumber =Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN but type of NAN is number
+//true=>1 false=>0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1=>true 0->false
+//""=>false
+//"Yash"=true
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+//console.log(typeof stringNumber);
+
+
+// *********************Operations*******************//
+
+
+let value =3 ;
+let negValue=-value;
+//console.log(value);
+
+
+let str1="Hello "
+let str2="Yash"
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+//console.log(true+); gives error
+// console.log(+"");
+
